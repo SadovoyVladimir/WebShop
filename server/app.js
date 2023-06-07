2e5d8d186b1bf4788bcfda7b8fce0dc347e2c39e
SadovoyVladimir <vla-sadovoj@mail.ru>
2023-06-07 15:57:19 +0300

    Добавление Docker

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
 const cors = require('cors')
-const initDatabase = require('./startUp/initDatabase')
+const path = require('path')
 const routes = require('./routes')
 
 const app = express()
@@ -15,6 +15,16 @@ app.use(cors())
 app.use('/api', routes)
 
 const PORT = config.get('port') ?? 8080
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client')))
+
+  const indexPath = path.join(__dirname, 'client', 'index.html')
+
+  app.get('*', (req, res) => {
+    res.sendFile(indexPath)
+  })
+}
 
 async function start() {
   try {
